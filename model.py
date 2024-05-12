4482f9a31c3f33592f71dc56797dfe86418c4c23
Salma-Reda-Moltaqa <SalmaRedaBedair>
2024-05-12 19:39:37 +0300

    edit model

--- model.py
+++ model.py
@@ -1,26 +1,38 @@
 import pandas as pd
-import warnings 
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
+import warnings
+warnings.filterwarnings('ignore')
 
 def predict_mobile_price_range(user_input):
-    warnings.filterwarnings('ignore')
+    # Load data
     df = pd.read_csv('train.csv')
-    x = df.drop(columns=['price_range','m_dep','fc','talk_time'],axis = 1)    
+    
+    # Cleaning
+    x = df.drop(columns=['price_range','m_dep','fc','talk_time'],axis=1)
     y = df['price_range']
-
     x_train, _, y_train, _ = train_test_split(x, y, test_size=0.2, random_state=42)
-
-    sv = SVC(kernel='linear', random_state=23)
-    sv.fit(x_train, y_train)
-
-    y_pred = sv.predict(user_input)
-
+    median = y_train.median()
+    y_train.fillna(median, inplace=True)
+    
+    # Pipeline
+    num_pipeline = Pipeline([('imputer', SimpleImputer(strategy="median"))])
+    x_prepared = num_pipeline.fit_transform(x_train)
+    
+    # Model
+    sv = SVC(kernel='rbf', random_state=23, degree=5, max_iter=-1)
+    sv.fit(x_prepared, y_train)
+    test_prepared = num_pipeline.transform(user_input)
+    y_pred = sv.predict(test_prepared)
+    
+    # Result
     if y_pred == 0:
-        return "50 - 300 $"
+        return "Mobile Price Range is 50 : 300 $"
     elif y_pred == 1:
-        return "300 - 600 $"
+        return "Mobile Price Range is 300 : 600 $"
     elif y_pred == 2:
-        return "600 - 900 $"
-    else:
-        return "900 - 1300 $"
+        return "Mobile Price Range is 600 : 900 $"
+    else: 
+        return "Mobile Price Range is 900 : 1300 $"
